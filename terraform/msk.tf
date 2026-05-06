dbaa7bc7c15f4b7a5b0e3f4e6ff73115e22276fe
Rsuthar26 <sutharripal26@gmail.com>
2026-05-06 13:59:37 +0100

    infra: enable msk.tf — 3 broker nodes eu-west-1, kafka.m5.large

--- terraform/msk.tf
+++ terraform/msk.tf
@@ -104,7 +104,7 @@ resource "aws_security_group" "msk" {
 resource "aws_msk_cluster" "main" {
   cluster_name           = "${var.project_name}-kafka"
   kafka_version          = "3.6.0"
-  number_of_broker_nodes = 2
+  number_of_broker_nodes = 3
 
   broker_node_group_info {
     instance_type = "kafka.m5.large"
